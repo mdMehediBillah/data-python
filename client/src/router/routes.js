83c1af92ff73178842f709feefa313eea56007a6
mdMehediBillah <mailbillah@gmail.com>
2025-02-04 08:55:05 +0100

    Not found page add

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,8 +1,15 @@
-import { AllData, FilteredData, GraphicalData, HomeCom } from "../components";
+import {
+  AllData,
+  FilteredData,
+  GraphicalData,
+  HomeCom,
+  NotFound,
+} from "../components";
 
 export const routes = [
   { path: "/", component: HomeCom },
   { path: "/all-data", component: AllData },
   { path: "/filtered-data", component: FilteredData },
   { path: "/graphical-data", component: GraphicalData },
+  { path: "/:pathMatch(.*)*", component: NotFound },
 ];

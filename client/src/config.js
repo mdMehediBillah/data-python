0a02a4c6800927c681b5fec5a4e95c84d1b926a3
Md Mehedi Billah <157976892+mdMehediBillah@users.noreply.github.com>
2025-02-06 16:40:53 +0100

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const config = {
   API_BASE_URL:
-    import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:5000/api/v1",
+    import.meta.env.VITE_API_BASE_URL || "https://data-python.onrender.com",
 };
 
 export default config;
